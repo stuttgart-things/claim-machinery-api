86336b5ba7f96d943d07f911a58116af994dde73
Patrick Hermann <patrick.hermann@sva.de>
2026-02-03 19:10:14 +0000

    feat: main

--- internal/claimtemplate/loader.go
+++ internal/claimtemplate/loader.go
@@ -1,7 +1,9 @@
 package claimtemplate
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
 	"gopkg.in/yaml.v3"
 )
@@ -12,9 +14,17 @@ func LoadClaimTemplate(path string) (*ClaimTemplate, error) {
 		return nil, err
 	}
 
+	if len(strings.TrimSpace(string(data))) == 0 {
+		return nil, fmt.Errorf("empty template file: %s", path)
+	}
+
 	var tmpl ClaimTemplate
 	if err := yaml.Unmarshal(data, &tmpl); err != nil {
 		return nil, err
+	}
+
+	if tmpl.Metadata.Name == "" {
+		return nil, fmt.Errorf("template missing metadata.name in %s", path)
 	}
 
 	return &tmpl, nil
